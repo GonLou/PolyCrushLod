2e818983fa5bd3c0735d6617fb885178a78e6dfb
GonLou <goncalo.lourenco@outlook.com>
2018-08-26 13:55:13 +0100

    rectified to index.html

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/test.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
